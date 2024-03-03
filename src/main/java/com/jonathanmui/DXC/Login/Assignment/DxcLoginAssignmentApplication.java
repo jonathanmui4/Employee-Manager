1d5b9f61993c9ee5b6c34ff89fc7a454ac6a2a52
Jonathan Mui <jonathanmui4@gmail.com>
2024-03-03 21:09:10 +0800

    Initial Commit

---
+++ src/main/java/com/jonathanmui/DXC/Login/Assignment/DxcLoginAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanmui.DXC.Login.Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxcLoginAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxcLoginAssignmentApplication.class, args);
+	}
+
+}
